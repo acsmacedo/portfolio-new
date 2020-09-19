81e683e38650952b433885aa0cf0da4aa8f218fb
acsmacedo <andy.carlos.souza@hotmail.com>
2020-09-19 17:35:38 -0300

    adicionei meu framework e comecei o about

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="home">
     <HomeHeader />
+    <HomeAbout />
   </div>
 </template>
 
 <script>
-import HomeHeader from './HomeHeader.vue'
+  import HomeHeader from './HomeHeader.vue'
+  import HomeAbout from './HomeAbout.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HomeHeader
+  export default {
+    name: 'Home',
+    components: {
+      HomeHeader,
+      HomeAbout
+    }
   }
-}
 </script>
-
-<style lang="scss" scoped>
-  .home {
-    display: flex;
-  }
-</style>
---
+++ src/components/Home/HomeAbout.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="home__about">
+    <div class=home__about__container>
+      <div>
+        <h1>
+          <span>Direto</span>
+          <span>ao</span>
+          <span>Ponto</span>
+        </h1>
+        <p>Olá, me chamo Anderson Macedo e sou desenvolvedor Front-end. Para conhecer mais sobre mim, acesse meu <a href="#">curriculo</a>.</p>
+        <p>O que eu posso adiantar? Sou detalhista, focado e atualmente estudo Ciências da Computação. Além disso, sou formado em Design, o que eu considero um grande diferencial quando desenvolvo projetos front-end.</p>
+        <p>Existem muitas coisas que poderia dizer sobre mim, mas vou deixar meus projetos pessoais falarem por si só....</p>
+      </div>
+      <img src="../../assets/illustrations/home-about.svg" alt="">
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'HomeAbout'
+  }
+</script>
